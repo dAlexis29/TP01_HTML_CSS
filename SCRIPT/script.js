af3e977c29e68e18db6f07b871aa95d1f24bd8b9
dAlexis29 <dicknguyenalexis@gmail.com>
2025-12-09 13:30:28 +0100

    commit caroussel

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,48 +1,18 @@
-(function () {
-    "use strict"
-    const slideTimeout = 5000;
-    const prev =  document.querySelector('#prev');
-    const next = document.querySelector('#next');
-    const $slides = document.querySelectorAll('slide');
-    let $dots;
-    let intervalId;
-    let currentSlide = 1;
-    function slideTo(index) {
-        currentSlide = index >= $slides.length || index < 1 ? 0 : index;
-        $slides.forEach($elt => $elt.style.transform = `translateX(-${currentSlide * 100}%)`);
-        $dots.forEach(($elt, key) => $elt.classList = `dot ${key === currentSlide? 'active': 'inactive'}`);
-    }
-    function showSlide() {
-        slideTo(currentSlide);
-        currentSlide++;
-    }
-    for (let i = 1; i <= $slides.length; i++) {
-        let dotClass = i == currentSlide ? 'active' : 'inactive';
-        let $dot = `<span data-slidId="${i}" class="dot ${dotClass}"></span>`;
-        document.querySelector('.carousel-dots').innerHTML += $dot;
-    }
-    $dots = document.querySelectorAll('.dot');
-    $dots.forEach(($elt, key) => $elt.addEventListener('click', () => slideTo(key)));
-    prev.addEventListener('click', () => slideTo(--currentSlide))
-    next.addEventListener('click', () => slideTo(++currentSlide))
-    intervalId = setInterval(showSlide, slideTimeout)
-    $slides.forEach($elt => {
-        let startX;
-        let endX;
-            clearInterval(intervalId);
-        }, false)
-        $elt.addEventListener('mouseout', () => {
-            intervalId = setInterval(showSlide, slideTimeout);
-        }, false);
-        $elt.addEventListener('touchstart', (event) => {
-            startX = event.touches[0].clientX;
-        });
-        $elt.addEventListener('touchend', (event) => {
-            endX = event.changedTouches[0].clientX;
-            if (startX > endX) {
-                slideTo(currentSlide + 1);
-            } else if (startX < endX) {
-                slideTo(currentSlide - 1);
-            }
-        });
-    })
+const images = document.querySelector('.carousel-images');
+const imgCount = document.querySelectorAll('.carousel-images img').length;
+
+let index = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+    index = (index + 1) % imgCount;
+    updateCarousel();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    index = (index - 1 + imgCount) % imgCount;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    images.style.transform = `translateX(${-index * 100}%)`;
+}
